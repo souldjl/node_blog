6dae61031700a51cfc7afdc86253bee2dc2223d0
daijiulong <344795953@qq.com>
2018-07-13 18:36:10 +0800

    接口1

--- app.js
+++ app.js
@@ -7,8 +7,3 @@ var app = express();
 app.get('/', function (req, res, next) {
     res.send('hello world')
 });
-
-
-app.listen(8888, function () {
-    console.log('app is listening at port 3000');
-});

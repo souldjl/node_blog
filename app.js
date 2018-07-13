79105c81b6117d5698f44b8205e55df746a9cabe
daijiulong <344795953@qq.com>
2018-07-13 18:13:07 +0800

    init

--- app.js
+++ app.js
@@ -4,10 +4,3 @@ var app = express();
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
-
-var server = app.listen(9000, function () {
-    var host = server.address().address;
-    var port = server.address().port;
-
-    console.log('Example app listening at http://%s:%s', host, port);
-});

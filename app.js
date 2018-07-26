e05f1880f905f97db7596921019a055a75772e98
daijiulong <344795953@qq.com>
2018-07-26 16:19:05 +0800

    x

--- app.js
+++ app.js
@@ -5,6 +5,24 @@ var superagent = require('superagent');
 var app = express();
 
 app.get('/', function (req, res, next) {
-    res.send('hello world')
+    superagent.get('https://cnodejs.org/')
+        .end(function (err, sres) {
+            if (err) {
+                return next(err);
+            }
+            var $ = cheerio.load(sres.text);
+            var items = [];
+            $('#topic_list .topic_title').each(function (idx, element) {
+                var $element = $(element);
+                items.push({
+                    title: $element.attr('title'),
+                    href: $element.attr('href')
+                });
+            });
+
+            res.send(items);
+        });
 });
+
+
 app.listen(process.env.PORT || 5000)
